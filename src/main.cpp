0e0dde44a3455fbbd09bf3c461d00dbbd5195021
Wiedjaja Atmadja <swiedjaja@gmail.com>
2023-08-26 11:41:43 +0700

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,11 @@
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(15, OUTPUT);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(15, HIGH);
   delay(100);
   digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(15, LOW);
   delay(1000);
 }
