f8da8194cc2ba0eee96b51243c1343a5f62a88fd
Wiedjaja Atmadja <swiedjaja@gmail.com>
2023-08-26 12:34:06 +0700

    Step2. add led red and green

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(15, OUTPUT);
+  pinMode(4, OUTPUT);
+
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(15, HIGH);
+  digitalWrite(4, LOW);
   delay(100);
   digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(15, LOW);
+  digitalWrite(4, HIGH);
   delay(1000);
 }
