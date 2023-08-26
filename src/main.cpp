8f7d8de8c6e0fde2cc099acd01ba5a1c9351eb89
Wiedjaja Atmadja <swiedjaja@gmail.com>
2023-08-26 15:06:10 +0700

    add dht11

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,41 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <Arduino.h>
+#include <DHTesp.h>
+
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 char ssid[] = "SteffN9";
 char pass[] = "steff123";
+DHTesp dht;
 
 #define LED_RED 15
 #define LED_GREEN 4
+#define DHT_PIN 5
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
+  dht.setup(DHT_PIN, DHTesp::DHT11);
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, 
   "blynk.cloud", 8080);
 }
 
+int nPrevTime = 0;
 void loop() {
+  int current = millis();
+  if ((current-nPrevTime)>5000)
+  {
+    nPrevTime = current;
+    float fHumidity = dht.getHumidity();
+    float fTemperature = dht.getTemperature();
+    Blynk.virtualWrite(V0, fHumidity);
+    Blynk.virtualWrite(V1, fTemperature);
+      Serial.printf("Humidity: %.2f, Temperature: %.2f\n",
+      fHumidity, fTemperature);
+  }
   Blynk.run();
 }
 
